2d12990ec0ecfed13d9fe06a35462fb30ffe90f4
Constantine <golovnev1306@mail.ru>
2021-08-09 08:01:47 +0700

    deleting useless files

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,5 +1,3 @@
 export { default as RoutesMap } from './RoutesMap'
-export { default as FormPasswordField } from './FormPasswordField'
-export { default as FormTextField } from './FormTextField'
 
 export default {}
